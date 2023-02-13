09791a21f0689f0976b6e9a60b9841cdbb4e838c
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-02-13 17:29:55 -0300

    feat/ added get customers controller to the route

--- src/routers/customers.routes.js
+++ src/routers/customers.routes.js
@@ -1,4 +1,5 @@
 import {Router} from 'express'
+import { getCustomersList } from '../controllers/getCustomersController.js'
 import { postClientController } from '../controllers/postClientController.js'
 import { putCustomerController } from '../controllers/putCustomerController.js'
 import { validatePostClient } from '../middlewares/validatePostClient.js'
@@ -6,7 +7,7 @@ import { validatePutCustomer } from '../middlewares/validatePutCustomer.js'
 
 const customersRouter = Router()
 
-customersRouter.get("/customers")
+customersRouter.get("/customers", getCustomersList)
 customersRouter.put("/customers/:id", validatePutCustomer, putCustomerController)
 customersRouter.post("/customers", validatePostClient, postClientController)
 
